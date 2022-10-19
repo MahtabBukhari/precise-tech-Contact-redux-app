f2a45014c3a064f86e049cb799fdcf1659a8de0d
MahtabBukhari <git config --global user.email you@example.com>
2022-10-19 14:37:07 +0500

    Create bootstrap theme in scss

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './layout/Navbar'
+import './styles/App.scss'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
